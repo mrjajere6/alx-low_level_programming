9a746216dd2a7a2f77a7e180ec955800361756c6
mrjajere6 <emjayy1738@gmail.com>
2022-03-16 07:30:44 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if char is greater than,
+ * is zero or is less than zero
+ * @n: receive an integer as an argument
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+
+		_putchar(45);
+	}
+	return (-1);
+}
