d19f01b387936bf49328108c76a785b7ec8955c5
mrjajere6 <emjayy1738@gmail.com>
2022-03-16 14:47:38 -0700

    adds twointegers together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds to integerss together
+ * and return its value
+ * @n: parameter it accepts (an integer)
+ * @x: 2nd parameter it accepts (also an integer)
+ * Return: n + x
+ */
+int add(int n, int x)
+{
+	return (n + x);
+}
