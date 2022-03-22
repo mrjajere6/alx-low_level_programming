c3746dd9eaa9772a7d6c2a767729d9a72bee0358
mrjajere6 <emjayy1738@gmail.com>
2022-03-22 10:56:43 -0700

    writing a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+/**
+ *_puts - prints string followed by newline.
+ *@str: pointer to string.
+ *
+ *Return: void.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		putchar(*str);
+		i++;
+		str++;
+	}
+	putchar('\n');
+}
