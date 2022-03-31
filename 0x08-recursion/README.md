7cbe7350ed6071a8704677d8e9616cd270c346e2
mrjajere6 <emjayy1738@gmail.com>
2022-03-31 15:59:37 -0700

    haut

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This projenct is about recursion in c programming language. Recursion is the ability of a function to keep calling itself until it reaches a ceertain criteria.
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -6,5 +7,3 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-int _putchar(char c);
